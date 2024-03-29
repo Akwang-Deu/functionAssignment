1dd5d636605cc36273dce8f5befea4b14c8ec878
Akwang-Deu <akwangdeu@gmail.com>
2024-03-29 11:49:35 +0300

    add files

---
+++ function.js
@@ -0,0 +1,23 @@
+function sorting(){
+    let names=["Mercy","Joy"];
+    console.log(names.sort());
+    console.log(names.reverse());
+}
+sorting()
+
+var array=[8,-2,4,-6];
+array.forEach (s=>{
+    if(s<0)
+    neg-count++;
+else
+post-count ++;
+});
+console.log(`post-count`);
+console.log(`neg-count`);
+
+
+let employee=[{id:1}, {name:"Akwang"},{salary:[600,400]}]
+employee.sort(function(a,b){
+    return a.salary-b.salary;
+})
+console.log(employee);
